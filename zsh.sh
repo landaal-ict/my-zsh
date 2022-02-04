11e449ddf7b2a8eea550a4908ad76aae4b30d460
landaal-ict <89043910+landaal-ict@users.noreply.github.com>
2022-02-04 11:46:21 +0100

    update my-zsh

--- zsh.sh
+++ zsh.sh
@@ -12,4 +12,4 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
 # echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
-cp -R .zshrc /$home/.zshrc
+cp -R .zshrc /$HOME/.zshrc
